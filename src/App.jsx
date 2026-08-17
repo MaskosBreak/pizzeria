2b8bf4ac2eed2b22113203c9fdc9bb6a970a0bcb
Masko <maskosbreak@gmail.com>
2026-08-17 12:40:04 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
