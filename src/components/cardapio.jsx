1ec8631093f4ded16879e48d4455895c8661b476
Masko <maskosbreak@gmail.com>
2026-08-20 11:16:20 -0300

    Add files via upload

---
+++ src/components/cardapio.jsx
@@ -0,0 +1,33 @@
+export default function Cardapio(){
+    return(
+        <section className="cardapio" id="cardapio">
+            <h2>Cardápio</h2>
+            <ul data-title="🍕 Tradicionais">
+                <li>Mussarela</li>
+                <li>Calabresa</li>
+                <li>Portuguesa</li>
+                <li>Marguerita</li>
+                <li>Frango com Catupiry</li>
+                <li>Quatro Queijos</li>
+                <li>Napolitana</li>
+                <li>Presunto e Queijo</li>
+                <li>Bacon</li>
+                <li>Milho com Bacon</li>
+            </ul>
+            <ul data-title="🍕 Especiais">
+                <li>Camarão com Catupiry</li>
+                <li>Carne Seca com Cream Cheese</li>
+                <li>
+                <li>
+                <li>
+                <li>
+                <li>
+                <li>
+                <li>
+                <li></li>
+                
+
+            </ul>
+        </section>
+    )
+}
